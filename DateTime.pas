ab209bc3f0c559343d9f78cd69fd056a992027cc
oleg <tupikin@sfedu.ru>
2021-10-21 09:18:14 +0300

    edit DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,4 @@
-﻿begin
-  
+﻿function IsLeapYear(Year:integer) := ((Year mod 4 = 0) and (not(Year mod 100 = 0))) or (Year mod 400 = 0);
+//function SecondInMinute(Minute: integer) := Minute *60;
+begin
 end.
